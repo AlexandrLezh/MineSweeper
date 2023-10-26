31e4dab38cc6771d3726b8ea04279ced6de371bb
AlexandrLezh <sasha_l@inbox.lv>
2023-10-26 12:25:04 +0300

    Add anonymous class paintComponent for drawing on panel

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -15,7 +15,13 @@ public class MineSweeper extends JFrame {
     }
 
     private void initPanel() {
-        panel= new JPanel();
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawLine(0,0,500,300);
+            }
+        };
         panel.setPreferredSize(new Dimension(500, 300));
         add(panel);
     }
