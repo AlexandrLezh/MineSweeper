92f6669c4e73daf378dbb550275fc6003cbba66e
AlexandrLezh <sasha_l@inbox.lv>
2023-10-26 11:35:15 +0300

    Minimal setup for app

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -4,4 +4,11 @@ public class MineSweeper extends JFrame {
     public static void main(String[] args) {
         new MineSweeper().setVisible(true);
     }
+
+    public MineSweeper() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Mine Sweeper");
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
