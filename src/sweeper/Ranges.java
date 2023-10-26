65eb7a59c3da6e62608195e09be4032b12cd09cf
AlexandrLezh <sasha_l@inbox.lv>
2023-10-26 20:29:43 +0300

    Added random bomb

--- src/sweeper/Ranges.java
+++ src/sweeper/Ranges.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Ranges {
     private static Coord size;
     private static ArrayList<Coord> allCoord;
-    private static Random random;
+    private static Random random = new Random();
 
     static void setSize(Coord size) {
         Ranges.size = size;
@@ -29,5 +29,9 @@ public class Ranges {
     public static boolean inRange(Coord coord) {
         return coord.x >= 0 && coord.x < size.x &&
                coord.y >= 0 && coord.y < size.y;
+    }
+
+    static Coord getRandomCoord() {
+        return new Coord(random.nextInt(size.x), random.nextInt(size.y));
     }
 }
