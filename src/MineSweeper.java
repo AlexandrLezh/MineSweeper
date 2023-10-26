8942a111ab1cd018d7f92ac1fb13c4d832030368
AlexandrLezh <sasha_l@inbox.lv>
2023-10-26 11:46:37 +0300

    Add panel to frame.

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,14 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MineSweeper extends JFrame {
+
+    JPanel panel;
+
     public static void main(String[] args) {
         new MineSweeper().setVisible(true);
     }
 
     public MineSweeper() {
+        panel= new JPanel();
+        panel.setPreferredSize(new Dimension(500, 300));
+        add(panel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Mine Sweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
 }
